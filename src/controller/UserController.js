9cc345b66841aed2f26cb213235b56baf2057c9e
phonghoang <hoangnghiaphong_t64@hus.edu.vn>
2024-05-24 16:22:03 +0700

    them thuc nang logout

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -136,7 +136,6 @@ const getDetailsUser = async (req, res) => {
     }
 }
 const refreshToken = async (req, res) => {
-    console.log("kaak", req.cookies.refresh_token)
     try {
         const token = req.cookies.refresh_token
         if(!token) {
@@ -155,7 +154,20 @@ const refreshToken = async (req, res) => {
         })
     }
 }
+const logoutUser= async (req, res) => {
+    try {
+        res.clearCookie("refresh_token")
+        return res.status(200).json({
+            status:"OK",
+            message:"Logout successfully"
+        })
 
+    } catch(e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
 module.exports = {
     createUser,
     loginUser,
@@ -163,5 +175,6 @@ module.exports = {
     deleteUser,
     getAllUser,
     getDetailsUser,
-    refreshToken
+    refreshToken,
+    logoutUser
 }
--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -5,6 +5,7 @@ const UserController = require('../controller/UserController')
 
 router.post('/sign-in', UserController.loginUser)
 router.post('/sign-up', UserController.createUser)
+router.post('/log-out', UserController.logoutUser)
 router.put('/update-user/:id', UserController.updateUser)
 router.delete('/delete-user/:id', authMiddleWare, UserController.deleteUser)
 router.get('/getAll',authMiddleWare, UserController.getAllUser)
